c1eb54bb0b3c35ddd093335e58575f910ef86f10
Stan Kozubenko <staspk@gmail.com>
2019-06-02 06:18:17 -0700

    AMAZING BREAKPOINT
    Main Page Done

--- js/siteScripts.js
+++ js/siteScripts.js
@@ -1,21 +1,33 @@
+var CUR_PAGE_OPEN = -1;
+
 var audioDiv = $("#audioControls");
 var introAudio =  new Audio('./../audio/intro-sound.mp3');
+var introOutSound =  new Audio('./../audio/intro-out.mp3');
 var mainAudio = new Audio('./../audio/main-song.mp3');
+var deepWhooshSound = new Audio('./../audio/deep-whoosh.mp3');
+var whooshEchoSound = new Audio('./../audio/whoosh-echo.mp3');
 var introPage  =  $('#introPage');
 var mainPage   =  $('#mainPage');
 
+var ewuLogo = $("#ewuLogo");
+var nameHolder = $("#nameHolder");
+var link1 = $("#link1");
+var link2 = $("#link2");
+var link3 = $("#link3");
+var link4 = $("#link4");
+
 audioDiv.toggle();
 mainPage.toggle();
-starfield.set_amount(2400);
+starfield.set_amount(2200);
 introAudio.play();
 
 
 function enterMainPage() {
     introAudio.play();
+    introOutSound.play();
     
-    animateCSS('#introPage', 'pulse', function() {
-        starfield.speed = 1200;
-
+    animateCSS('#introPage', 'intro-disappear', function() {
+        starfield.speed = 1400;
 
         mainAudio.addEventListener('ended', function() {
             this.currentTime = 0;
@@ -25,43 +37,45 @@ function enterMainPage() {
 
         introPage.toggle();
         mainPage.toggle();
-        $("#ewuLogo").toggle();
-        $("#link1").toggle();
-        $("#link2").toggle();
-        $("#link3").toggle();
-        $("#link4").toggle();
-        // $("#firstName-mainPage").toggle();
-        // $("#lastName-mainPage").toggle();
-        // $("#firstName-mainPage").toggle();
-        // $("#lastName-mainPage").toggle();
+        ewuLogo.toggle();
+        nameHolder.toggle();
+        link1.toggle();
+        link2.toggle();
+        link3.toggle();
+        link4.toggle();
         
-        $("#ewuLogo").show('slide', {direction: 'left'}, 100, function() {
-            starfield.speed = 800;
+        $("#nameHolder").fadeToggle('slow');
+        deepWhooshSound.play();
+        $("#ewuLogo").show('slide', {direction: 'left'}, 110, function() {
+            starfield.speed = 1100;
             starfield.set_amount(2000);
-            $("#link1").show('slide', {direction: 'left'}, 75, function() {
-                starfield.speed = 600;
+            $("#link1").show('slide', {direction: 'left'}, 60, function() {
+                starfield.speed = 900;
                 starfield.set_amount(1700);
                 $("#link2").show('slide', {direction: 'left'}, 75, function() {
-                    starfield.speed = 400;
+                    starfield.speed = 780;
                     starfield.set_amount(1500);
-                    $("#link3").show('slide', {direction: 'left'}, 75, function() {
-                        starfield.speed = 300;
+                    whooshEchoSound.play();
+                    $("#link3").show('slide', {direction: 'left'}, 100, function() {
+                        starfield.speed = 500;
                         starfield.set_amount(1250);
-                        $("#link4").show('slide', {direction: 'left'}, 75, function() {
-                            starfield.speed = 200;
+                        
+                        $("#link4").show('slide', {direction: 'left'}, 120, function() {
+                            starfield.speed = 250;
                             starfield.set_amount(1000);
                         });
                     });
                 });
             });
         });
-        
-
     });
     
-    
+    CUR_PAGE_OPEN = 1;
 }
 
 
-
+function openPage(openingPage) {    //pageNum 0 = mainPage, 1-4 in order of appearence of UI
+    
+    
+}
 
