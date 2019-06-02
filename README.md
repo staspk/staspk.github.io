284f1fb679009c4b5239108a638ac1b0741768c9
Stan Kozubenko <staspk@gmail.com>
2019-06-02 04:05:52 -0700

    Added Music. Among many other things

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # The Stanislav Kozubenko Website
 ## Audio Sources
-* Intro Sound: https://www.youtube.com/watch?v=MT7MEpM2Muw
+* Intro Sound: https://www.youtube.com/watch?v=MT7MEpM2Muw.
+* Main Song: https://www.youtube.com/watch?v=HI7egq3SnIA.
 
 ## Image Source
-* Eagle: https://www.ewu.edu/
+* Eagle: https://www.ewu.edu/.
 
 ## Libraries/Code Used
 * Bootstrap / Jquery
-* ColorRotate Effect: http://jsfiddle.net/P6WVg/7/
+* ColorRotate Effect: http://jsfiddle.net/P6WVg/7/.
+* Javascript Stars: https://codepen.io/john052/pen/KzIfb.
--- js/siteScripts.js
+++ js/siteScripts.js
@@ -1,29 +1,55 @@
 var audioDiv = $("#audioControls");
 var introAudio =  new Audio('./../audio/intro-sound.mp3');
+var mainAudio = new Audio('./../audio/main-song.mp3');
 var introPage  =  $('#introPage');
 var mainPage   =  $('#mainPage');
 
 audioDiv.toggle();
 mainPage.toggle();
+starfield.set_amount(10000);
+introAudio.play();
 
 
 function enterMainPage() {
     introAudio.play();
-
+    
     animateCSS('#introPage', 'pulse', function() {
-        introPage.toggle();
+        starfield.set_amount(1000);
 
+        mainAudio.addEventListener('ended', function() {
+            this.currentTime = 0;
+            this.play();
+        }, false);
+        mainAudio.play();
+
+        introPage.toggle();
         mainPage.toggle();
         $("#ewuLogo").toggle();
+        $("#link1").toggle();
+        $("#link2").toggle();
+        $("#link3").toggle();
+        $("#link4").toggle();
         // $("#firstName-mainPage").toggle();
         // $("#lastName-mainPage").toggle();
         // $("#firstName-mainPage").toggle();
         // $("#lastName-mainPage").toggle();
         
         $("#ewuLogo").show('slide', {direction: 'left'}, 100);
-        
+        $("#link1").show('slide', {direction: 'left'}, 75, function() {
+            $("#link2").show('slide', {direction: 'left'}, 75, function() {
+                $("#link3").show('slide', {direction: 'left'}, 75, function() {
+                    $("#link4").show('slide', {direction: 'left'}, 75, function() {
+
+                    });
+                });
+            });
+        });
 
     });
     
     
 }
+
+
+
+
