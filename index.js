e905c2e66cc5a43be724307d059eff62ff4c9e59
Stanislav Kozubenko <staspk@gmail.com>
2025-03-07 13:21:20 -0800

    Improved naming, changed from default events of a to css

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
-var mainAudio = new Audio('./audio/main-song.mp3');
-var buttonHoverSound = new Audio('./audio/button-hover.mp3');
-var twinkleSound = new Audio('./audio/twinkle-sound.mp3');
+const mainAudio = new Audio('./audio/main-song.mp3');
+
+const buttonHoverSound = new Audio('./audio/button-hover.mp3');
+const twinkleSound = new Audio('./audio/twinkle-sound.mp3');
 
 const introView      =  $('#view-intro');
-const homeView       =  $('#view-home');    const ewuLogo = $("#ewuLogo"); const nameDiv = $("#nameDiv"); const link1 = $("#link1"); const link2 = $("#link2"); const link3 = $("#link3"); const link4 = $("#link4");
+const homeView       =  $('#view-home');    const ewuLogo = $("#ewu-logo"); const nameDiv = $("#nameDiv"); const link1 = $("#link1"); const link2 = $("#link2"); const link3 = $("#link3"); const link4 = $("#link4");
 const workexpView    =  $('#view-workexp');
 const educationView  =  $('#view-education');
 const projectsView   =  $('#view-projects');
 const contactView    =  $('#view-contact');
 
 
-$(".hover-sound").hover(function () {
+$(".hover-sound").hover(() => {
     buttonHoverSound.play();
-}).mouseleave(function ()
+}).mouseleave(() =>
 {
     buttonHoverSound.pause();
     buttonHoverSound.currentTime = 0;
 });
 
-$(".twinkle-sound").hover(function () {
+$(".twinkle-sound").hover(() => {
     twinkleSound.play();
 });
 
 
-function enterMainPage() {
-    // const ewuLogo = $("#ewuLogo");
-    // const nameHolder = $("#nameDiv");
-    // const link1 = $("#link1");
-    // const link2 = $("#link2");
-    // const link3 = $("#link3");
-    // const link4 = $("#link4");
+function openMainView() {
     let introAudio =  new Audio('./audio/intro-sound.mp3');
     let introOutSound =  new Audio('./audio/intro-out.mp3');
     let deepWhooshSound = new Audio('./audio/deep-whoosh.mp3');
@@ -39,10 +34,10 @@ function enterMainPage() {
     introAudio.play();
     introOutSound.play();
     
-    animateCSS('#view-intro', 'intro-exit', function() {
+    animateCSS('#view-intro', 'intro-exit', () => {
         setStarField(1400);
 
-        mainAudio.addEventListener('ended', function() {        //This event listener restarts the song when it ends
+        mainAudio.addEventListener('ended', () => {        // This event listener restarts the song when it ends
             this.currentTime = 0;
             this.play();
         }, false);
@@ -59,34 +54,31 @@ function enterMainPage() {
         
         nameDiv.fadeToggle('slow');
         deepWhooshSound.play();
-        ewuLogo.show('slide', {direction: 'left'}, 110, function() {
+        whooshEchoSound2.play();
+        ewuLogo.show('slide', {direction: 'left'}, 110, () => {
             setStarField(1100, 2000);
-            link1.show('slide', {direction: 'left'}, 60, function() {
+            link1.show('slide', {direction: 'left'}, 60, () => {
                 setStarField(900, 1700);
                 rhythmicEchoSound.play();
-                link2.show('slide', {direction: 'left'}, 75, function() {
+                link2.show('slide', {direction: 'left'}, 75, () => {
                     setStarField(780, 1500);
                     whooshEchoSound1.play();
-                    link3.show('slide', {direction: 'left'}, 100, function() {
+                    link3.show('slide', {direction: 'left'}, 100, () => {
                         setStarField(500, 1250);
-                        link4.show('slide', {direction: 'left'}, 120, function() {
+                        link4.show('slide', {direction: 'left'}, 120, () => {
                             setStarField(250, 1000);
                         });
                     });
                 });
             });
         });
-        whooshEchoSound2.play();
+        
     });
     
     CUR_PAGE_OPEN = 1;
 }
 
-function closeMainPage(event, linkClicked) {
-    event.preventDefault();
-
-    const ewuLogo = $("#ewuLogo");
-    // const nameHolder = $("#nameHolder");
+function closeMainPage(linkClicked) {
     const linkFadeOutSound1 = new Audio('./audio/link-fade-out-sound.mp3');
     const linkFadeOutSound2 = new Audio('./audio/link-fade-out-sound.mp3');
     const logoSlideOutSound = new Audio('./audio/logo-slide-out-sound.mp3');
@@ -99,22 +91,22 @@ function closeMainPage(event, linkClicked) {
 
     let clickedLink = $("#link" + linkClicked);
     clickedLink.css("color", "#fcabcd");
-    clickedLink.hide('puff', 500, function() {
+    clickedLink.hide('puff', 500, () => {
         clickedLink.css("color", "white");
     });
 
     linkFadeOutSound1.play();
-    $("#link" + links[0]).hide('explode', {direction: 'left'}, 200, function() {
+    $("#link" + links[0]).hide('explode', {direction: 'left'}, 200, () => {
         setStarField(800, 1500);
         linkFadeOutSound2.play(); 
         logoSlideOutSound.play();
-        $("#link" + links[1]).hide('explode', {direction: 'left'}, 200, function() {
+        $("#link" + links[1]).hide('explode', {direction: 'left'}, 200, () => {
             setStarField(1000, 1700);
-            $("#link" + links[2]).hide('explode', {direction: 'left'}, 100, function() {
+            $("#link" + links[2]).hide('explode', {direction: 'left'}, 100, () => {
                 setStarField(1200, 2000);
-                nameDiv.hide('drop', {direction: 'right'}, 110, function() {
+                nameDiv.hide('drop', {direction: 'right'}, 110, () => {
                     setStarField(1000, 1800);
-                    ewuLogo.hide('drop', {direction: 'left'}, 110, function() {
+                    ewuLogo.hide('drop', {direction: 'left'}, 110, () => {
                         setStarField(800, 1600);
                         homeView.toggle();
                         openPage(linkClicked);
@@ -126,31 +118,25 @@ function closeMainPage(event, linkClicked) {
 }
 
 function reopenMainPage() {
-    const ewuLogo = $("#ewuLogo");
-    // const nameHolder = $("#nameHolder");
-    const link1 = $("#link1");
-    const link2 = $("#link2");
-    const link3 = $("#link3");
-    const link4 = $("#link4");
-    var deepWhoosh = new Audio('./audio/deep-whoosh.mp3');
-    var invertPopIn1 = new Audio('./audio/inverted-whoosh-popin.mp3');
-    var invertPopIn2 = new Audio('./audio/inverted-whoosh-popin.mp3');
+    const deepWhoosh = new Audio('./audio/deep-whoosh.mp3');
+    const invertPopIn1 = new Audio('./audio/inverted-whoosh-popin.mp3');
+    const invertPopIn2 = new Audio('./audio/inverted-whoosh-popin.mp3');
 
     setStarField(1500, 2200);
     homeView.toggle();
-    ewuLogo.show('drop', {direction: 'left'}, 110, function() {
+    ewuLogo.show('drop', {direction: 'left'}, 110, () => {
         deepWhoosh.play();
-        nameDiv.show('drop', {direction: 'right'}, 110, function() {
+        nameDiv.show('drop', {direction: 'right'}, 110, () => {
             setStarField(1300, 2000);
             invertPopIn1.play();
-            link4.show('explode', {direction: 'left'}, 100, function() {
+            link4.show('explode', {direction: 'left'}, 100, () => {
                 setStarField(1200, 1700);
-                link3.show('explode', {direction: 'left'}, 100, function() {
+                link3.show('explode', {direction: 'left'}, 100, () => {
                     setStarField(900, 1500);
                     invertPopIn2.play();
-                    link2.show('explode', {direction: 'left'}, 100, function() {
+                    link2.show('explode', {direction: 'left'}, 100, () => {
                         setStarField(600, 1250);
-                        link1.show('explode', {direction: 'left'}, 100, function() { 
+                        link1.show('explode', {direction: 'left'}, 100, () => { 
                             setStarField(250, 1000);
                         });
 
@@ -164,11 +150,9 @@ function reopenMainPage() {
 }
 
 
-// Function needs to be heavily reworked.
-    // Animations/Sounds needs to be changed for pages 1-3
 function openPage(pageNum) {    //pageNum 0 = homeView, 1-4 in order of appearence of UI
 
-    var pageToShow;
+    let pageToShow;
     switch(pageNum) {
         case 1: pageToShow = workexpView; break;
         case 2: pageToShow = educationView; break;
@@ -180,14 +164,16 @@ function openPage(pageNum) {    //pageNum 0 = homeView, 1-4 in order of appearen
 
     if(pageNum == 1 || pageNum == 2 || pageNum == 3 || pageNum == 4) {
         setStarField(1400, 2100);
-        new Audio('./audio/spacey-canvas-unfurl.mp3').play();
-        pageToShow.show('fold', 1000, function() {
+        let canvasUnfurl = new Audio('./audio/spacey-canvas-unfurl.mp3');
+        canvasUnfurl.volume = 0.1;
+        canvasUnfurl.play();
+        pageToShow.show('fold', 1000, () => {
             setStarField(750, 1400);
             new Audio('./audio/gmail-new-mail-sound.mp3').play();
-            setTimeout(function () {
+            setTimeout(() => {
                 setStarField(500, 1200);
                 
-                setTimeout(function () {
+                setTimeout(() => {
                     setStarField(250, 1000);
                 }, 100);
             }, 100);
@@ -198,13 +184,13 @@ function openPage(pageNum) {    //pageNum 0 = homeView, 1-4 in order of appearen
         setStarField(1400, 2100);
         new Audio('./audio/spacey-canvas-unfurl.mp3').play();
         pageToShow.addClass("moveDownFadeIn");
-        pageToShow.show('fold', 1000, function() {
+        pageToShow.show('fold', 1000, () => {
             pageToShow.removeClass("moveDownFadeIn");
             setStarField(750, 1400);
             new Audio('./audio/gmail-new-mail-sound.mp3').play();
-            setTimeout(function () {
+            setTimeout(() => {
                 setStarField(500, 1200);
-                setTimeout(function () {
+                setTimeout(() => {
                     setStarField(250, 1000);
                     
                 }, 100);
@@ -216,7 +202,7 @@ function openPage(pageNum) {    //pageNum 0 = homeView, 1-4 in order of appearen
 
 function closePage(pageNum) {
 
-    var pageToClose;
+    let pageToClose;
     switch(pageNum) {
         case 1: pageToClose = workexpView; break;
         case 2: pageToClose = educationView; break;
@@ -227,10 +213,10 @@ function closePage(pageNum) {
 
     setStarField(600, 1500);
     pageToClose.addClass("moveUpFadeOut");    
-    setTimeout(function () {    
+    setTimeout(() => {    
         setStarField(1200, 1700);
             new Audio("./audio/inverted-spacey-canvas-unfurl.mp3").play();
-            pageToClose.hide("fold", 1000, function() {
+            pageToClose.hide("fold", 1000, () => {
                 setStarField(1400, 2000);
                 pageToClose.removeClass("moveUpFadeOut");
                 reopenMainPage();
